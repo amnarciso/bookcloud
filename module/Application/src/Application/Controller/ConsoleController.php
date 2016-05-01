c0370a865e0b34aa83ec647060d125a2732622c2
Andre <amnarciso@gmail.com>
2016-05-01 11:26:09 -0300

    hot fix 04 on test print lists of books and users

--- module/Application/src/Application/Controller/ConsoleController.php
+++ module/Application/src/Application/Controller/ConsoleController.php
@@ -43,7 +43,7 @@ class ConsoleController extends AbstractActionController {
 		//Print list of new books
 		echo "\nLIST OF BOOKS:\n";
 		foreach ($bookList as $book) {
-			echo $book['title'] . "\n";
+			echo "\t" . $book['title'] . "\n";
 		}
 
 		//Get list of subscribed users
@@ -52,7 +52,7 @@ class ConsoleController extends AbstractActionController {
 		//Print list of subscribed users
 		echo "\nLIST OF EMAILS:\n";
 		foreach ($userList as $user) {
-			echo $user['email'] . "\n";
+			echo "\t" . $user['email'] . "\n";
 		}
 
 		//Send mails if there is at least one new book
