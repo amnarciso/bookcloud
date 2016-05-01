b23ee2fedf1deebe709850aba489554264ca104c
Andre <amnarciso@gmail.com>
2016-05-01 11:21:22 -0300

    test print lists of books and users

--- module/Application/src/Application/Controller/ConsoleController.php
+++ module/Application/src/Application/Controller/ConsoleController.php
@@ -22,29 +22,42 @@ use Application\Model\Users;
  *
  */
 class ConsoleController extends AbstractActionController {
+	echo "********************\n";
+	echo "sendmailAction START\n";
+	echo date('Y-m-d H:i:s e') . "\n";
+	echo "********************\n";
 
 	public function sendmailAction() {
-		echo 'start' . PHP_EOL;
-		print "start print\n";
-
 		//Initialize variables
         $sm = $this->getServiceLocator();
         $books = new Books($sm);
         $users = new Users($sm);
 		$mailler = new Mailler($sm);
 
+		//Get new books from last week
 		$lastDate = date("o-m-d", date_sub(new DateTime(),date_interval_create_from_date_string("7 days"))->getTimestamp());
 		$lastDate = '2016-02-01';
-		var_dump($lastDate);
 		$bookList = $books->listBooks(array("sql" => "date >= '$lastDate'"))->toArray();	
 		
-		$userList = $users->listUsers("subscribe = 1")->toArray();
-		var_dump($userList);
+		//Print list of new books
+		echo "\nLIST OF BOOKS:\n"
+		foreach ($books as $book) {
+			echo $book['title'];
+		}
 
-//		if (count($bookList) > 0){
-//			$mailler->newsletterEmail($userList, $bookList, date("d/m/o"));
-//		}
-				
+		//Get list of subscribed users
+		$userList = $users->listUsers("subscribe = 1")->toArray();
+
+		//Print list of subscribed users
+		echo "\nLIST OF EMAILS:\n"
+		foreach ($userList as $user) {
+			echo $user['email'];
+		}
+
+		//Send mails if there is at least one new book
+		if (count($bookList) > 0){
+			$mailler->newsletterEmail($userList, $bookList, date("d/m/o"));
+		}
 		die();
 	}
 }
